02569528e5bef22567d933e576c498f1968d6f61
jhkim31 <biz.jh.kim@gmail.com>
2022-10-26 16:11:53 +0900

    FEAT: python-json 파서 추가

---
+++ DataParse/Python/jsonParser.py
@@ -0,0 +1,31 @@
+import json
+
+a = {"key1" : "value1", "key2" : 2}
+
+"""
+1. json(dict) => 문자열
+"""
+jsonStr = json.dumps(a)
+print(type(jsonStr), jsonStr)
+
+"""
+2. json(dict) => 파일(문자열)
+실행 경로에 파일 생성
+"""
+with open('./test.test', 'w') as fp:
+    json.dump(a, fp)
+
+
+"""
+3. 문자열 => json(dict)
+"""
+jsonData = json.loads(jsonStr)
+print(type(jsonData), jsonData)
+
+"""
+4. 파일(문자열) => json(dict)
+실행 경로에 파일 생성
+"""
+with open('./test.test', 'r') as fp:
+    jsonData = json.load(fp)
+    print(type(jsonData), jsonData)
